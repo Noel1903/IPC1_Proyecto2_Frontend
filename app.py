24850187ab28ce9941d458974796b1489fb74948
Noel1903 <osmar19lemus@gmail.com>
2020-10-25 18:18:19 -0600

    subiendo archivos

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask import render_template
+from flask_cors import CORS
 app=Flask(__name__)
-
+CORS(app)
 @app.route('/')
 def index():
     return render_template('Index.html')
@@ -12,6 +13,11 @@ def registro():
 
 @app.route('/Admin')
 def admin():
-    return render_template('InterfazAdmin.html')    
+    return render_template('InterfazAdmin.html')  
+
+@app.route('/Cliente')
+def cliente():
+    return render_template('InterfazClient.html')  
+
 if  __name__ == "__main__":
     app.run(debug=True,port=5000)
